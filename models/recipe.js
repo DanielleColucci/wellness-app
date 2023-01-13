d26ef3b4b9db7e5e0b34adbf19a538f0c08c594a
DanielleColucci <ddcolucci99@gmail.com>
2023-01-13 15:35:35 -0500

    build reviewSchema, recipeSchema; compile and export Recipe model

---
+++ models/recipe.js
@@ -0,0 +1,35 @@
+import mongoose, { Mongoose } from "mongoose"
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  owner: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  content: String, 
+  rating: {
+    type: Number, 
+    required: true,
+    min: 1, 
+    max: 5,
+    default: 5
+  }
+}, {
+  timestamps: true
+})
+
+const recipeSchema = new Schema({
+  name: {type: String, required: true},
+  owner: {type: Schema.Types.ObjectId, ref:'Profile'},
+  estTime: Number,
+  meal: {type: String, enum: ['Breakfast', 'Lunch', 'Dinner', 'Other']},
+  ingredients: {type: Schema.Types.ObjectId, ref: 'Ingredient'},
+  reviews: [reviewSchema],
+  instructions: String
+}, {
+  timestamps: true
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema)
+
+export {
+  Recipe
+}
